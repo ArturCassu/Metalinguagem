85f888612985a1542c0bb559969201a0223563d1
ArturCass <artur.jalles@picpay.com>
2024-05-17 14:34:46 -0300

    nao foi

--- drag.js
+++ drag.js
@@ -1,35 +1,50 @@
-const code_blocks = document.querySelectorAll('.code-block');
-const dragbox = document.querySelector('.dragbox');
+const dragbox = document.getElementById("dragbox");
 
-const blocks_position = []
-
-code_blocks.forEach(block => {
-
-    block.setAttribute('draggable', 'true');
-
-    blocks_position.push([block.offsetTop, block.offsetLeft]);
-
-    block.addEventListener('dragstart', (e) => {
-        block.classList.add('dragging');
-    });
-    block.addEventListener('dragend', (e) => {
-        console.log(e);
-        blocks_position.forEach((position, index) => {
-            
-        });
-
-        block.classList.remove('dragging');
-    });
+const blocks_positions = [];
+const initial_blocks = dragbox.innerText.trim().split(" ");
+dragbox.innerHTML = "";
+dragbox.addEventListener("mouseover", (e) => {
+  moveCursor(e.layerX, e.layerY);
 });
 
-function getTwoClossestBlocks() {
-    blocks_position.forEach((position, index) => {
-        
-    });
+populateBox(initial_blocks);
 
+const cursor = document.createElement("div");
+cursor.id = "cursor";
+cursor.innerText = "|";
+dragbox.appendChild(cursor);
 
-    return [index1, index2];
-
+function createBlock(text) {
+  const block = document.createElement("div");
+  block.className = "code-block";
+  block.draggable = true;
+  block.innerText = text;
+  return block;
 }
 
-console.log(blocks_position);
+function populateBox(list) {
+  list.forEach((item) => {
+    const block = createBlock(item);
+    dragbox.appendChild(block);
+    blocks_positions.push([block.offsetLeft, block.offsetTop]);
+  });
+}
+
+function moveCursor(x, y) {
+  dragbox.removeChild(cursor);
+  let minY = blocks_positions[0][1];
+  let minX = blocks_positions[0][0];
+  blocks_positions.forEach((position, index) => {
+    if (
+      Math.abs(position[1] - y) <= minY &&
+      Math.abs(position[0] - x) <= minX
+    ) {
+      console.log(position, index);
+      console.log(minY, minX);
+      console.log(y, x);
+      minY = Math.abs(position[1] - y);
+      minX = Math.abs(position[0] - x);
+      dragbox.insertBefore(cursor, dragbox.children[index]);
+    }
+  });
+}
