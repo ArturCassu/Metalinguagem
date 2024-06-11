b78656888ceb968c7f09ec8b1b4a0545d0a4f9b3
arthur13stalberg09silva07 <arthur.stalberg@germinare.org.br>
2024-06-11 13:05:40 -0300

    add coisas legais

--- getelements.js
+++ getelements.js
@@ -1,4 +1,5 @@
 const campoKeyUp = document.querySelector("#campo-keyup");
+const campoKeyUp3 = document.querySelector("#campo-keyup3");
 const html = document.querySelectorAll(".div-getelements2");
 const span1 = document.querySelectorAll(".span1");
 const span2 = document.querySelectorAll(".span2");
@@ -42,5 +43,32 @@ campoKeyUp.addEventListener("keyup", () => {
       span2[i].classList.remove("errado");
       span3[i].classList.remove("errado");
     }
+  }
+})
+
+
+// document.getElementById
+campoKeyUp.addEventListener("keyup", () => {
+  const valCampo3 = campoKeyUp3.value.toLowerCase();
+  const tamanhoCampo3 = valCampo3.length;
+
+  for (let i = 0; i < html.length; i++) {
+    console.log(i);
+    let valProduto = html[i].textContent.toLowerCase();
+    valProduto = valProduto.substring(0, tamanhoCampo3);
+
+
+
+    if (valCampo3 == "divlegal") {
+      span1[i].classList.add("certa");
+      span3[i].classList.remove("errado");
+      yupi.play();
+      // pergunta.innerHTML =
+      //   "Tente capturar todos os elementos que estão presentes na divmuitolegal";
+    } else if (valCampo3 == "divmuitolegal") {
+      span1[i].classList.remove("certa");
+      span3[i].classList.add("errado");
+      erro.play()
+    } 
   }
 })
